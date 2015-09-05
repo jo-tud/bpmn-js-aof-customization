dbf941fef405e82796c0b6e1aec08a067975d3a9
Korbinian Hörfurter <korbi_@web.de>
2015-09-05 11:58:56 +0200

    Set up base structure of the PerformerChooser

--- app/performerChooser/PerformerChooser.js
+++ app/performerChooser/PerformerChooser.js
@@ -1,3 +1,29 @@
 /**
  * Created by Korbi on 04.09.2015.
  */
+
+'use strict';
+
+var  forEach = require('lodash/collection/forEach'),
+    filter = require('lodash/collection/filter');
+
+function PerformerChooser(popupMenu){
+
+    function getOptions(){
+        var list;
+
+        list={'option1','option2'};
+        return list;
+    }
+
+    this.openChooser(position,entries){
+        var entries = getOptions(element);
+
+        popupMenu.open('replace-menu', position, entries);
+    }
+
+}
+
+PerformerChooser.$inject = [ 'popupMenu' ];
+
+module.exports = PerformerChooser;
