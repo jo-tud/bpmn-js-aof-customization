43971e8e9460869525eb02757ecdeb6d3cd67af0
Korbinian Hörfurter <korbi_@web.de>
2015-09-09 11:39:26 +0200

    Removed artifacts

--- app/aof-customization/overrideModules/CustomReplace.js
+++ app/aof-customization/overrideModules/CustomReplace.js
@@ -93,14 +93,12 @@ function CustomBpmnReplace(bpmnFactory, moddle, popupMenu, replace, selection, m
         var newProps=moddle.getElementDescriptor(businessObject).propertiesByName;
         forEach(oldProps, function(prop,key){
             if(oldBusinessObject.hasOwnProperty(key) && newProps.hasOwnProperty(key)){
-                if(['id','name','loopCharacteristics'].indexOf(key)==-1){
+                if(['id'].indexOf(key)==-1){
                     businessObject[key]=oldBusinessObject[key];
                 }
             }
 
         });
-        businessObject.name = oldBusinessObject.name;
-        businessObject.loopCharacteristics = oldBusinessObject.loopCharacteristics;
 
         newElement = replace.replaceElement(element, newElement);
 
