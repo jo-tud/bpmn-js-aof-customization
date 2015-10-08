dad4f4a024bd03c3ad7488bfbc35d5258431d725
khörfurter <korbinian.hoerfurter@tu-dresden.de>
2015-10-08 15:06:35 +0200

    Added  deactivated custom rules provider for implementing https://github.com/korbinianHoerfurter/bpmn-js-aof-customization/issues/26

--- app/aof-customization/index.js
+++ app/aof-customization/index.js
@@ -3,8 +3,9 @@ var Modules = {
     bpmnReplace: ['type', require('./overrideModules/CustomReplace.js')],
     contextPadProvider: ['type', require('./overrideModules/CustomContextPadProvider.js')],
 //ExtensionModules
-    __init__: ['appAssigner'],
-    appAssigner: ['type', require('./extensionModules/AppAssigner.js')]
+    __init__: ['appAssigner'],//,'customRules'],
+    appAssigner: ['type', require('./extensionModules/AppAssigner.js')],
+   // customRules: ['type', require('./extensionModules/CustomRules.js')]
 };
 
 module.exports = Modules;
