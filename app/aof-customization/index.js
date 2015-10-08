8e61d788b86120882ab203132db89e86b6ffa0b0
khörfurter <korbinian.hoerfurter@tu-dresden.de>
2015-10-08 14:04:58 +0200

    Removed now unused custom Modules

--- app/aof-customization/index.js
+++ app/aof-customization/index.js
@@ -1,7 +1,5 @@
 var Modules = {
     // OverrideModules
-    renderer: ['type', require('./overrideModules/ExtendedBpmnRenderer.js')],
-    pathMap: ['type', require('./overrideModules/ExtendedPathMap.js')],
     bpmnReplace: ['type', require('./overrideModules/CustomReplace.js')],
     contextPadProvider: ['type', require('./overrideModules/CustomContextPadProvider.js')],
 //ExtensionModules
